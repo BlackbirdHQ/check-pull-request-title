a3899465641e92ecc38472021372426ae72462ac
Martin Jesper Low Madsen <mj@blackbird.online>
2020-01-31 13:09:52 +0100

    Reference the latest pull request rather than the initial check payload

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 
-function run() {
+async function run() {
   const pattern = core.getInput('pattern');
+  const token = core.getInput('token');
+
   const regex = new RegExp(pattern);
-  const title = github.context.payload?.pull_request?.title;
+
+  const octokit = new github.GitHub(token);
+
+  const { data: pullRequest } = await octokit.pulls.get({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    pull_number: github.context.payload?.pull_request?.number!,
+  });
+
+  const title = pullRequest.title;
 
   core.info(title);
 
